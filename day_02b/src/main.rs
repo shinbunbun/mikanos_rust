8038f7ad433ae0ff985c1569ee033b342a712ae3
shinbunbun <takamuneyuto@gmail.com>
2022-07-23 09:23:39 +0900

    open_fileを追加

--- day_02b/src/main.rs
+++ day_02b/src/main.rs
@@ -13,7 +13,7 @@ use core::{
 use uefi::{
     prelude::*,
     proto::media::{
-        file::{Directory, File, FileAttribute, FileHandle, FileMode, RegularFile},
+        file::{Directory, File, FileAttribute, FileHandle, FileMode, FileType, RegularFile},
         fs::SimpleFileSystem,
     },
     table::boot::{self, MemoryDescriptor, MemoryMapKey},
@@ -24,20 +24,6 @@ use uefi::{
 #[used]
 #[no_mangle]
 pub static _fltused: i32 = 0;
-
-// https://github.com/rust-lang/rust/issues/51540#issue-332112999
-#[lang = "oom"]
-// https://github.com/rust-lang/rust-analyzer/issues/4490#issuecomment-1074922003
-#[cfg(not(test))]
-extern "C" fn oom(_: core::alloc::Layout) -> ! {
-    loop {}
-}
-
-#[panic_handler]
-#[cfg(not(test))]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
 
 // https://github.com/skoji/laranja-os/blob/osbook_day02b/src/main.rs#L38
 fn get_memory_map(system_table: &mut SystemTable<Boot>) {
@@ -56,20 +42,84 @@ fn get_memory_map(system_table: &mut SystemTable<Boot>) {
     }
 }
 
-/* fn open_file(handle:&Handle,boot: &BootServices,file_path:&str,mode:FileMode)->RegularFile{
-    let loaded_image = boot.handle_protocol::<LoadedImage>(*handle).unwrap_success().get();
-    let device=unsafe {(*loaded_image).device()};
-    let file_system = boot.handle_protocol::<SimpleFileSystem>(device).unwrap_success().get();
-    let mut root_dir: Directory = unsafe {(*file_system).open_volume().unwrap_success()};
-    let file_handle = root_dir.open(file_path,mode,FileAttribute::empty()).unwrap_success();
-    unsafe {RegularFile::new(file_handle)}
-}  */
+fn open_file(system_table: &mut SystemTable<Boot>, file_path: &str) -> FileType {
+    write(system_table.stdout(), format_args!("1\n")).unwrap();
+    let sfs = if let Ok(sfs) = system_table
+        .boot_services()
+        .locate_protocol::<SimpleFileSystem>()
+    {
+        unsafe { &mut *sfs.get() }
+    } else {
+        write(
+            system_table.stdout(),
+            format_args!("no simple filesystem protocol\n"),
+        )
+        .unwrap();
+        panic!("no sfs");
+    };
+    write(system_table.stdout(), format_args!("2\n")).unwrap();
+
+    let mut directory = match sfs.open_volume() {
+        Ok(x) => x,
+        Err(err) => {
+            write(
+                system_table.stdout(),
+                format_args!("open volume failed: {:?}\n", err),
+            )
+            .unwrap();
+            panic!("open volume failed");
+        }
+    };
+    write(system_table.stdout(), format_args!("3\n")).unwrap();
+
+    let filename_buf: &mut [u16] = &mut [0; 256];
+    write(system_table.stdout(), format_args!("4\n")).unwrap();
+
+    let file_name = match uefi::CStr16::from_str_with_buf(file_path, filename_buf) {
+        Ok(file_name) => file_name,
+        Err(err) => {
+            write(
+                system_table.stderr(),
+                format_args!("Convert CStr16 error: {:?}\n", err),
+            )
+            .unwrap();
+            panic!("Convert CStr16 error");
+        }
+    };
+    write(system_table.stdout(), format_args!("5\n")).unwrap();
+
+    let file = match directory.open(file_name, FileMode::CreateReadWrite, FileAttribute::empty()) {
+        Ok(file) => file,
+        Err(err) => {
+            write(
+                system_table.stderr(),
+                format_args!("open file failed: {:?}\n", err),
+            )
+            .unwrap();
+            panic!("open file failed");
+        }
+    };
+    write(system_table.stdout(), format_args!("6\n")).unwrap();
+
+    file.into_type().unwrap()
+}
 
 #[entry]
 fn main(_handle: Handle, mut system_table: SystemTable<Boot>) -> Status {
+    if let Err(err) = uefi_services::init(&mut system_table) {
+        write(system_table.stdout(), format_args!("Init Err: {:?}\n", err)).unwrap();
+    };
+
     write(system_table.stdout(), format_args!("Hello, world!\n")).unwrap();
+
     get_memory_map(&mut system_table);
+
     write(system_table.stdout(), format_args!("100\n")).unwrap();
+
+    open_file(&mut system_table, "memmap");
+
+    write(system_table.stdout(), format_args!("101\n")).unwrap();
+
     /* let mmap_buf: &mut [u8] = &mut [0; 4096];
     if let Err(err) = system_table.boot_services().memory_map(mmap_buf) {
         write(
